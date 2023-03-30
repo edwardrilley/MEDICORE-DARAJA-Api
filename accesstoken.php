a2bea98d644e1d9e0dd35c92f38be7d84f81c2de
edward <edwardrilley@gmail.com>
2023-03-30 14:57:30 +0300

    upload

--- accesstoken.php
+++ accesstoken.php
@@ -1,18 +1,35 @@
 <?php
 
-$consumerKey ="aE15jGTAHGS2CroObdAiRFSiec7ONGoG";
-$consumerSecret ="A3wcEEqEuHUoGXF6";
+$consumerKey = "aE15jGTAHGS2CroObdAiRFSiec7ONGoG";
+$consumerSecret = "A3wcEEqEuHUoGXF6";
+
 // MPESA ACCESS TOCKEN URL
 $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-$headers = ['Content-Type:application/json; charset=utf8'];
+
+$headers = ['Content-Type: application/json; charset=utf8'];
+
 $curl = curl_init($access_token_url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
 curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+
 $result = curl_exec($curl);
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-$result = json_decode($result);
-echo $access_token = $result->access_token;
+if (!$result) {
+  die("Connection Failure");
+}
+
+$response = json_decode($result);
+
+if (isset($response->access_token)) {
+  $access_token = $response->access_token;
+  echo $access_token;
+} else {
+  die("Invalid response from server");
+}
+
 curl_close($curl);
+
+?> 
